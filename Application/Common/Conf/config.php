6fd711f14e07b11c5e196b7a83c59559e28d6165
yuji <yuji@qq.com>
2018-05-31 21:21:45 +0800

    hom

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,9 +3,9 @@ return array(
     //'配置项'=>'配置值'
     //数据库配置信息
     'DB_TYPE'   => 'mysql', // 数据库类型
-     'DB_HOST'   => '192.168.1.236', // 服务器地址
-     'DB_NAME'   => 'meinv_data', // 数据库名
-     'DB_USER'   => 'jishu', // 用户名
+     'DB_HOST'   => 'localhost', // 服务器地址
+     'DB_NAME'   => 'meitu_data', // 数据库名
+     'DB_USER'   => 'root', // 用户名
      'DB_PWD'    => '123456', // 密码
 //     'DB_HOST'   => 'localhost', // 服务器地址
 //     'DB_NAME'   => 'jkcms_data', // 数据库名
